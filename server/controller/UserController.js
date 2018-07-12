a39479dbb77084e299a597869eb7f52554ea5013
MinuteWang <243560619@qq.com>
2018-07-12 18:39:42 +0800

    'register'

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,9 +1,24 @@
 const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
+const uuid = require('uuid-js')
 const UserModel = require('../models/UserModel')
 
+const Joi = require('joi')
+const md5 = crypto.createHash('md5')
+
+// 定义验证逻辑
+const LoginSchema = Joi.object().keys({
+  password: Joi.string().required(),
+  accout: Joi.string().required()
+})
+
+const RegisterSchema = LoginSchema.keys({
+  type: Joi.number().required()
+})
+
 const Login = async function (ctx, next) {
-  const md5 = crypto.createHash('md5')
+  const { error } = Joi.validate(ctx.request.body, LoginSchema)
+  ctx.assert(!error, 400, error.details[0].message)
   const password = ctx.request.body.password
   const secretPw = md5.update(password).digest('hex')
   const user = await UserModel.findUser({ ...ctx.request.body, password: secretPw })
@@ -13,9 +28,20 @@ const Login = async function (ctx, next) {
 }
 
 const Register = async function (ctx, next) {
-
+  const { error } = Joi.validate(ctx.request.body, RegisterSchema)
+  ctx.assert(!error, 400, error.details[0].message)
+  const password = ctx.request.body.password
+  const user = {
+    uuid: uuid.create(),
+    accout: ctx.request.body.accout,
+    type: ctx.request.body.type,
+    password: md5.update(password).digest('hex')
+  }
+  await UserModel.createUser(user)
+  next()
 }
 
 module.exports = {
-  Login
+  Login,
+  Register
 }
