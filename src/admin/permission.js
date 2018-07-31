259d8cf1d12f4c34faa609799277641aad899260
MinuteWang <243560619@qq.com>
2018-07-31 22:28:38 +0800

    permission

---
+++ src/admin/permission.js
@@ -0,0 +1,16 @@
+import router from './router';
+import store from './store';
+import NProgress from 'nprogress'; // Progress 进度条
+import 'nprogress/nprogress.css'; // Progress 进度条样式
+import { GetRequest } from './utils';
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  if (store.state.token) {
+  } else {
+  }
+});
+
+router.afterEach(() => {
+  NProgress.done(); // 结束Progress
+});
